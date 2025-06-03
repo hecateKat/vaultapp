baf013d0f4cb0b1be223a9d7486f62a234856088
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-06-03 14:46:05 +0200

    Initial commit

---
+++ src/test/java/com/kat/vaultapp/VaultappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kat.vaultapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
